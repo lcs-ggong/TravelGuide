ac5042412660faa4f2e394088e91bcbaee86522a
Grace Gong <ggong@lcs.on.ca>
2023-02-06 20:20:18 -0500

    Initial Commit

---
+++ TravelGuide/TravelGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelGuideApp.swift
+//  TravelGuide
+//
+//  Created by Grace Gong on 2023-02-06.
+//
+
+import SwiftUI
+
+@main
+struct TravelGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
