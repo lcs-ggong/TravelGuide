17e00a1ee66a8b83e4bae228cdc6126257a61b21
Grace Gong <ggong@lcs.on.ca>
2023-02-08 19:13:33 -0500

    finish the gender

--- TravelGuide/Views/SexSelection.swift
+++ TravelGuide/Views/SexSelection.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct SexSelection: View {
+    
+    @State var isFemale = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            
+            Divider()
+                .padding(.vertical, 10)
+            
+            Text("Sex")
+                .bold()
+                .font(.title)
+                .padding(.bottom, 3)
+            
+            Text("Choose your sex")
+                .italic()
+                .padding(.bottom, 10)
+            
+            
+            
+            HStack{
+                // Example usage #1 - Menu
+                Picker(selection: $isFemale,
+                       label: Text("Picker Name"),
+                       content: {
+                    
+                    Text("Male").tag(false)
+                    Text("Female").tag(true)
+
+                    
+                })
+                .pickerStyle(.segmented)
+            }
+            
+            Text("isFemale is: \(isFemale ? "true" : "false")")
+            
+        }
+        .padding()
     }
 }
 
 struct SexSelection_Previews: PreviewProvider {
     static var previews: some View {
-        SexSelection()
+       SexSelection()
     }
 }
